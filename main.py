ac6d2dd98d8d7cbaec6f8e568bbbf165fe283666
Tristan <Tristan.Casemajor@gmail.com>
2023-08-15 21:38:17 +0200

    new splashscreen

--- main.py
+++ main.py
@@ -20,9 +20,9 @@ class Gui(Widget):
         self.splashscreen = SplashScreen()
         self.add_widget(self.splashscreen)
         self.main_gui = MainTabbedPanel()
-        self.main_gui.opacity  = 0
+        self.main_gui.opacity = 0
         self.add_widget(self.main_gui)
-        Clock.schedule_once(self.remove_splashscreen, 4)
+        Clock.schedule_once(self.remove_splashscreen, 6)
     def on_size(self, *args):
         self.splashscreen.size = self.size
         self.main_gui.size = self.size
@@ -55,12 +55,14 @@ class SplashScreen(Widget):
         super().__init__(**kwargs)
         with self.canvas:
             self.rectangle_background = Rectangle(pos=self.pos, size=self.size, source="images/background_splashscreen.png")
-        self.image_splashscreen = Image(source="images/logo_dev_icon_editor.jpg", size=(dp(150), dp(150)))
+        self.image_splashscreen = Image(source="images/logo_dev_icon_editor_without_background.png", size=(dp(170), dp(170)))
+        self.loading_anim = Image(source="images/loading_anim.gif", size=(dp(220), dp(220)), anim_delay=1/33)
         self.add_widget(self.image_splashscreen)
+        self.add_widget(self.loading_anim)
     def on_size(self, *args):
         self.rectangle_background.size = self.size
         self.image_splashscreen.pos = (self.center_x-self.image_splashscreen.width/2, self.center_y-self.image_splashscreen.height/2)
-
+        self.loading_anim.pos = self.center_x-self.loading_anim.width/2, self.center_y/20
 class DevImageEditApp(App):
 
     def on_start(self):
